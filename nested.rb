d137bd34dd91e7e09da03924a81cb7a0e34e27a5
Travis Desir <travisgdesir31@gmail.com>
2018-09-20 17:19:00 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -82,6 +82,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+
+    programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+    :languages => ["LISP", "C"]
  }
 end
 
@@ -102,7 +106,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     programmer_hash[:alan_kay][:known_for] = "GUI"
+     alans_new_info = "GUI"
      
      
 end
